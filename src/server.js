b9dd7d43ea34e2e58a98f94b3f6b62326abe2a68
LudSkop <luda.skopenko80@gmail.com>
2026-05-28 15:47:09 +0300

    added format

--- src/server.js
+++ src/server.js
@@ -73,8 +73,7 @@ app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.json({ message: `Retrieved note with ID: ${noteId}` });
 });
-
-app.get('/test-error', () => {
+app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
 });
 
