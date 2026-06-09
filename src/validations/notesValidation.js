a6cca30e1627eeb4eb6abf56c7a27db1e53525bc
LudSkop <luda.skopenko80@gmail.com>
2026-06-09 16:41:22 -0400

    fix createNoteSchema

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -5,7 +5,7 @@ import { isValidObjectId } from 'mongoose';
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(20),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(''),
   }),
@@ -32,7 +32,7 @@ export const noteIdSchema = {
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string().default('').allow(''),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
 };
